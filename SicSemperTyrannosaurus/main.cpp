d70e164dab3211815c149cb9b74bdd5ab46b1725
Pedro Esteves <up201705160@fe.up.pt>
2019-10-19 19:50:57 +0100

    Fixed Sic Semper Tyrannosaurus

--- SicSemperTyrannosaurus/main.cpp
+++ SicSemperTyrannosaurus/main.cpp
@@ -21,7 +21,50 @@ int main() {
         if(beginning == 'Q'){
             cin >> l >> r;
             unsigned long int vr;
-            for(unsigned int )
+            #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    unsigned long int n, k, q;
+    cin >> n >> k >> q;
+    
+    unsigned long int vi;
+    vector<unsigned long int> v;
+    while(n > 0){
+        cin >> vi;
+        v.push_back(vi);
+        n--;
+    }
+    char beginning;
+    unsigned long int l, r, p, x; 
+    while(q > 0){
+        cin >> beginning;
+        if(beginning == 'Q'){
+            cin >> l >> r;
+            
+            for(unsigned long int i = 0; i < k; i++){
+                for(unsigned long int j = l; j < r; j++){
+                    v[j] += v[j-1];
+                }
+            }
+            cout << v[r-1]%1000000007 << endl;
+            for(unsigned long int i = 0; i < k; i++){
+                for(unsigned long int j = r-1; j >= l; j--){
+                    v[j] -= v[j-1];
+                }
+            }
+        }
+        else{ //'U'
+            cin >> p >> x;
+            v[p-1] = x;
+        }
+        q--;
+    }
+    
+    return 0;
+}
         }
         else{ //'U'
             cin >> p >> x;
