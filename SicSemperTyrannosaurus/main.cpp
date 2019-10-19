5810a36d89b24952c38dfdfaf3af8c76987d2763
Pedro Esteves <up201705160@fe.up.pt>
2019-10-19 19:49:18 +0100

    Added Concentratio Game and Sic Semper Tyrannosaurus solutions

---
+++ SicSemperTyrannosaurus/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    unsigned int n, k, q;
+    cin >> n >> k >> q;
+    
+    unsigned long int vi;
+    vector<unsigned long int> v
+    while(n > 0){
+        cin >> vi;
+        v.push_back(vi);
+        n--;
+    }
+    char beginning;
+    unsigned long int l, r, p, x; 
+    while(q > 0){
+        cin >> beginning;
+        if(beginning == 'Q'){
+            cin >> l >> r;
+            unsigned long int vr;
+            for(unsigned int )
+        }
+        else{ //'U'
+            cin >> p >> x;
+            v[p] = x;
+        }
+        q--;
+    }
+    cout << a + b;
+    return 0;
+}
